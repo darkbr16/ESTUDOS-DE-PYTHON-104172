2b3316cc8ae9f8001f145a69cb4b560ebbf6f499
darkbr16 <phsandesferreira.10@gmail.com>
2026-08-21 16:46:43 -0300

    Add files via upload

---
+++ 6-antecessor_sucessor.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+
+print("=solicitando dados=")
+valor=int(input('digite seu numero:'))
+
+antecessor=valor-1
+sucessor=valor+1
+print("\n=exibindo dados=")
+
+print("o numero antecessor de",valor,"e",antecessor)
+print('o numero sucessor de',valor,'e',sucessor)
