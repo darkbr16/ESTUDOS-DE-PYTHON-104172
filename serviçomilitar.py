5f167e99561ceee6868e1b1553e5249ddfbc3bc9
darkbr16 <phsandesferreira.10@gmail.com>
2026-09-09 16:43:17 -0300

    Update gender input and condition for military service

--- serviçomilitar.py
+++ serviçomilitar.py
@@ -1,9 +1,9 @@
 import os
 os.system ('cls')
-sexo=input('digite seu sexo:')
+sexo=input('digite seu sexo (M OU F):').upper()
 idade=int (input('digite sua idade:'))
 
-if sexo=='masculino' and idade >= 18:
+if sexo=='M' and idade >= 18:
     serviço=('voce precisa servir ao exercito')
 else:
     serviço=('voce nao precisa servir ao exercito')
