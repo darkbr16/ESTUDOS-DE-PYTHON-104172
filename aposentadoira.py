81991d6222b29092a6dc9b05b80bd92722974551
darkbr16 <phsandesferreira.10@gmail.com>
2026-09-09 14:16:05 -0300

    Update retirement eligibility calculation
    Added calculation for age based on birth year.

--- aposentadoira.py
+++ aposentadoira.py
@@ -1,7 +1,10 @@
 import os
+from datetime import date
 os.system ('cls')
+codigo=int(input('digite seu codigo'))
 nome=input('digite seu nome:')
-idade=int(input('digite sua idade:'))
+ano_de_nascimento=int(input('digite seu ano de nascimento:'))
+idade=date.today().year-ano_de_nascimento
 tempoclt=int(input('digite  seu tempo trabalhado:'))
 
 if idade >=65 or tempoclt >=30:
