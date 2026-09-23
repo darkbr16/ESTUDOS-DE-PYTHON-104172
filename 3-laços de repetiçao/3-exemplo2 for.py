c00b957724f44d82f47a3b9d385c87f67f6cebd4
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 14:57:03 -0300

    atividades de laço de repetiçao -for.

---
+++ 3-laços de repetiçao/3-exemplo2 for.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+
+for i in range(1,4):
+    input('digite seu nome:')
+    input(f"pergunta {i}")
+print(('fim'))
---
+++ 3-laços de repetiçao/7-for exercicio contagem regressiva.py
@@ -0,0 +1,12 @@
+import os
+import time
+os.system ('cls')
+
+
+numero1= int (input('digite um numero:'))
+
+for i in range(numero1,0,-1):
+
+    print (i)
+    time.sleep(60)
+print ('KABOOM')
